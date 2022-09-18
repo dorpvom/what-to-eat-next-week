ad0b7eb0e0bb23078e1aead3319faf9a51903850
dorp <dorp@cs.uni-bonn.de>
2022-09-18 13:59:06 +0200

    blacked + new tested functions in picker

--- wtenw/picker/resolver.py
+++ wtenw/picker/resolver.py
@@ -1,14 +1,42 @@
 import json
 from pathlib import Path
+from random import shuffle
 
 FOOD = json.loads((Path(__file__).parent.parent / 'food' / 'food.json').read_text())
+
+
+class DishPlan:
+    def __init__(self):
+        self.dishes = []
+        self.major_items = []
+        self.ingredients = []
+
+    def add_dish(self, dish):
+        self.dishes.append(dish)
+        self.major_items.extend(FOOD['dishes'][dish])
+        self.ingredients.extend(resolve_ingredients(dish))
+
 
 class UnknownDish(Exception):
     pass
 
 
+def add_dish_if_not_redundant(dish: str, plan: DishPlan) -> None:
+    if dish not in FOOD['dishes']:
+        raise UnknownDish(f'{dish} not found in list of known dishes')
+    major_items = FOOD['dishes'][dish]
+    if all(item not in plan.major_items for item in major_items):
+        plan.add_dish(dish)
+
+
+def fetch_dishes_and_create_order() -> list[str]:
+    dishes = list(FOOD['dishes'].keys())
+    shuffle(dishes)
+    return dishes
+
+
 def resolve_ingredients(dish: str) -> list[str]:
-    if not dish in FOOD['dishes']:
+    if dish not in FOOD['dishes']:
         raise UnknownDish(f'{dish} not found in list of known dishes')
     ingredients = []
     for dish_part in FOOD['dishes'][dish]:
